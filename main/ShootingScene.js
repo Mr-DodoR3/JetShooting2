333f9501118a6c6a06868561c59dcbd36e7806b8
RyogaNishihan <dodo2020.1007@gmail.com>
2024-01-19 23:52:48 +0900

    ver0,23

--- main/ShootingScene.js
+++ main/ShootingScene.js
@@ -3,7 +3,8 @@ class ShootingScene extends GameScene {
     super("shootingScene");
 
     this.player = {
-      image : null
+      image : null,
+      pos : { x: DISPLAY_WIDTH / 2, y: DISPLAY_HEIGHT / 2 + 200 }
     };
   }
 
@@ -18,16 +19,32 @@ class ShootingScene extends GameScene {
   create() {
     super.create();
 
-    this.player.image = this.add.image(DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2 + 200, UNIT_DATA[selectAircraft].tag);
+    this.player.image = this.add.image(this.player.pos.x, this.player.pos.y, UNIT_DATA[selectAircraft].tag);
     this.player.image.scaleX = this.player.image.scaleX * 0.5;
     this.player.image.scaleY = this.player.image.scaleY * 0.5;
   }
 
   update() {
+    this.player.image.setX(this.player.pos.x);
+    this.player.image.setY(this.player.pos.y);
     super.update();
   }
 
   contller() {
-
+    if (this.cursors.up.isDown) {
+      this.player.pos.y -= 5;
+    }
+    if (this.cursors.down.isDown) {
+      this.player.pos.y += 5;
+    }
+    if (this.cursors.right.isDown) {
+      this.player.pos.x += 5;
+    }
+    if (this.cursors.left.isDown) {
+      this.player.pos.x -= 5;
+    }
+    if (this.space.isDown) {
+      
+    }
   }
 }
